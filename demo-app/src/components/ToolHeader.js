5829ec00da92c4c04ecb639d81ac03d879c5bb52
Eric Greene <eric@t4d.io>
2021-02-03 12:44:22 -0500

    add exercise 10

--- demo-app/src/components/ToolHeader.js
+++ demo-app/src/components/ToolHeader.js
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 
 export const ToolHeader = ({ headerText }) => {
-
   return (
     <header>
       <h1>{headerText}</h1>
     </header>
   );
-
 };
 
 ToolHeader.propTypes = {
