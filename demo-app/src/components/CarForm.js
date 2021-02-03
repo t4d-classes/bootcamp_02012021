4d644ca3b0a9276df870af1a160a0636f4410414
Eric Greene <eric@t4d.io>
2021-02-02 19:37:53 -0500

    add car form

---
+++ demo-app/src/components/CarForm.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import { toString } from '../utils';
+
+const getInitCarForm = () => ({
+  make: '', model: '', year: NaN, color: '', price: 0
+});
+
+export const CarForm = (props) => {
+
+  const [carForm, setCarForm] = useState(getInitCarForm());
+
+  const change = (e) => {
+    setCarForm({
+      ...carForm,
+      [e.target.name]: e.target.getAttribute('data-type') === 'number'
+        ? parseInt(e.target.value, 10) : e.target.value,
+    });
+  };
+
+  const submitCar = () => {
+    props.onSubmitCar({
+      ...carForm,
+    });
+
+    setCarForm(getInitCarForm());
+  };
+
+  return (
+    <form>
+        <div>
+          <label htmlFor="make-input">Make</label>
+          <input type="text" id="make-input" name="make"
+            value={carForm.make} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="model-input">Model</label>
+          <input type="text" id="model-input" name="model"
+            value={carForm.model} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="year-input">Year</label>
+          <input type="text" data-type="number" id="year-input" name="year"
+            value={toString(carForm.year)} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="color-input">Color</label>
+          <input type="text" id="color-input" name="color"
+            value={carForm.color} onChange={change} />
+        </div>
+        <div>
+          <label htmlFor="price-input">Price</label>
+          <input type="text" data-type="number" id="price-input" name="price"
+            value={toString(carForm.price)} onChange={change} />
+        </div>
+        <button type="button" onClick={submitCar}>{props.buttonText}</button>
+      </form>
+  )
+
+};
+
+CarForm.defaultProps = {
+  buttonText: 'Submit Car',
+};
+
+CarForm.propTypes = {
+  buttonText: PropTypes.string,
+  onSubmitCar: PropTypes.func.isRequired,
+};
--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -1,61 +1,31 @@
 import { useState } from 'react';
 
-import { toString } from '../utils';
 import { CarTable } from './CarTable';
+import { CarForm } from './CarForm';
 
 export const CarTool = (props) => {
 
-  const [carForm, setCarForm] = useState({
-    make: '', model: '', year: 1900, color: '', price: 0
-  });
+  const [ cars, setCars ] = useState([ ...props.cars ]);
 
-  const change = (e) => {
+  const addCar = (car) => {
 
-    console.log(e.target.getAttribute('data-type'));
-
-    setCarForm({
-      ...carForm,
-      [e.target.name]: e.target.getAttribute('data-type') === 'number'
-        ? parseInt(e.target.value, 10) : e.target.value,
-    });
-
+    setCars([
+      ...cars,
+      {
+        ...car,
+        id: Math.max(...cars.map(c => c.id), 0) + 1,
+      },
+    ]);
   };
 
-  console.log(carForm);
 
   return (
     <>
       <header>
         <h1>Car Tool</h1>
       </header>
-      <CarTable cars={props.cars} />
-      <form>
-        <div>
-          <label htmlFor="make-input">Make</label>
-          <input type="text" id="make-input" name="make"
-            value={carForm.make} onChange={change} />
-        </div>
-        <div>
-          <label htmlFor="model-input">Model</label>
-          <input type="text" id="model-input" name="model"
-            value={carForm.model} onChange={change} />
-        </div>
-        <div>
-          <label htmlFor="year-input">Year</label>
-          <input type="text" data-type="number" id="year-input" name="year"
-            value={toString(carForm.year)} onChange={change} />
-        </div>
-        <div>
-          <label htmlFor="color-input">Color</label>
-          <input type="text" id="color-input" name="color"
-            value={carForm.color} onChange={change} />
-        </div>
-        <div>
-          <label htmlFor="price-input">Price</label>
-          <input type="text" data-type="number" id="price-input" name="price"
-            value={toString(carForm.price)} onChange={change} />
-        </div>
-      </form>
+      <CarTable cars={cars} />
+      <CarForm buttonText="Add Car" onSubmitCar={addCar}  />
     </>
   );
 
